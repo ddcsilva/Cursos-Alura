432f4c2c9a76f0f062f2f0ec3d4e3d78153993ae
Danilo Silva <danilo.silva@msn.com>
2021-04-26 23:10:54 -0300

    Exemplo 01 - Primeiro Projeto

---
+++ C#/Primeiros Passos/AprendendoCSharp/01-PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio!");
+        }
+    }
+}
