3b7dc66bbbe2a191a7a34c9a0bfccc6813eb8849
Danilo Silva <danilo.silva@msn.com>
2021-04-27 22:16:30 -0300

    Exemplo 04 - Conversão e outros tipos numéricos

---
+++ C#/Primeiros Passos/AprendendoCSharp/04-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".:: Projeto 04 ::.");
+
+            double salario = 1200.50;
+            Console.WriteLine(salario);
+
+            // O int é um tipo de variável que suporte valores até 32 bits
+            // Conversão em Casting. Neste caso, perde a precisão
+            int salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            // O long é um tipo de variável que suporte valores até 64 bits
+            long idade = 1300000000000000;
+            Console.WriteLine(idade);
+
+            // O short é um tipo de variável que suporte valores até 16 bits
+            short quantidadeProdutos = 150;
+            Console.WriteLine(quantidadeProdutos);
+
+            // O float é um tipo de variável de ponto flutuante de menor precisão
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
