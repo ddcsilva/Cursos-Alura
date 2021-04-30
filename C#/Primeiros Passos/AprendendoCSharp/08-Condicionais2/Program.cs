fcb94c8e239255a05bc74badfcac520fd66bd5c1
Danilo Silva <danilo.silva@msn.com>
2021-04-29 21:25:00 -0300

    Exemplo 8 - Condicionais 2

---
+++ C#/Primeiros Passos/AprendendoCSharp/08-Condicionais2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".:: Projeto 08 ::.");
+
+            int idadeJoao = 18;
+            // int quantidadePessoas = 2;
+            // bool acompanhado = quantidadePessoas >= 2;
+            bool acompanhado = false;
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+            Console.WriteLine("A execução acabou. Tecle <Enter> para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
