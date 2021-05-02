e8d696f62eec8682633f3d04df07b80b34b34224
Danilo Silva <danilo.silva@msn.com>
2021-05-02 01:33:13 -0300

    Aula 01 - Nossa primeira classe

---
+++ C#/Orientação a Objetos/ByteBank/01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_ByteBank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ C#/Orientação a Objetos/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ContaCorrente();
+        }
+    }
+}
