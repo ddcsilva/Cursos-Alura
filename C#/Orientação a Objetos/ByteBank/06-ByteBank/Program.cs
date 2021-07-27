0d9846d1f91c0e60e7b9ab9671ced133fa256bed
Danilo Silva <danilo.silva@msn.com>
2021-07-27 14:00:24 -0300

    Método Sem Retorno

---
+++ C#/Orientação a Objetos/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+            Console.WriteLine(resultadoSaque);
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            contaDoBruno.Depositar(500);
+
+            Console.WriteLine(contaDoBruno.saldo);
+        }
+    }
+}
