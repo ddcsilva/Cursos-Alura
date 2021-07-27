0735386c203af23e296a50d3535dd9d82a0ef787
Danilo Silva <danilo.silva@msn.com>
2021-07-27 12:18:23 -0300

    Usando Nossa Classe

---
+++ C#/Orientação a Objetos/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Conta: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Novo Saldo: " + contaDaGabriela.saldo);
+        }
+    }
+}
