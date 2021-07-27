ac1647db5a1b0ff7a3caf01ff76a533053e89716
Danilo Silva <danilo.silva@msn.com>
2021-07-27 12:58:04 -0300

    Nossa Primeira Função

---
+++ C#/Orientação a Objetos/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+
+            System.Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+            System.Console.WriteLine(resultadoSaque);
+
+            System.Console.WriteLine(contaDoBruno.saldo);
+        }
+    }
+}
