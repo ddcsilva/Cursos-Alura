6a404a6d2f523e0db161714b70d45c774e21d76c
Danilo Silva <danilo.silva@msn.com>
2021-07-27 12:30:34 -0300

    Valores Padrões

---
+++ C#/Orientação a Objetos/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Gabriela";
+            conta.saldo = 200;
+
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.saldo);
+        }
+    }
+}
