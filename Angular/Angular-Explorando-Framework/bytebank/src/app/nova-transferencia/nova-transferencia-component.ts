a489d977fd92c94a8c819cefefce93af2461c8ad
Danilo Silva <danilo.silva@msn.com>
2023-01-17 12:07:59 -0300

    Exportando dados com output

--- Angular/Angular-Explorando-Framework/bytebank/src/app/nova-transferencia/nova-transferencia-component.ts
+++ Angular/Angular-Explorando-Framework/bytebank/src/app/nova-transferencia/nova-transferencia-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,12 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia-component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTranferir = new EventEmitter<any>();
   valor: number = 0;
   destino: number = 0;
 
   trasferir() {
     console.log('Solicitada uma nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorParaEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTranferir.emit(valorParaEmitir);
   }
 }
