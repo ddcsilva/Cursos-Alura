06c37b4c70b6be52d1d2718e9c50cdd114e006b9
Danilo Silva <danilo.silva@msn.com>
2023-01-18 20:03:02 -0300

    O primeiro objeto

--- CSharp/CSharp-Orientacao-a-Objetos/Bytebank/Bytebank/Program.cs
+++ CSharp/CSharp-Orientacao-a-Objetos/Bytebank/Bytebank/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank;
+
+ContaCorrente contaDoDanilo = new ContaCorrente();
+contaDoDanilo.titular = "Danilo Silva";
+contaDoDanilo.numero_agencia = 15;
+contaDoDanilo.conta = "1010-X";
+contaDoDanilo.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Danilo = " + contaDoDanilo.saldo);
