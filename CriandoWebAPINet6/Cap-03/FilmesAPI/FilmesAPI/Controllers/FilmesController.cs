02061272d8be2fe3bf9d84c79133d630e95c4285
danilo.silva <danilo.silva@msn.com>
2023-11-04 23:00:43 -0300

    Paginando Resultados

--- CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
+++ CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -20,9 +20,11 @@ public class FilmesController : Controller
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes() // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
+    // Skip: Parâmetro que indica quantos elementos devem ser ignorados no início da lista.
+    // Take: Parâmetro que indica quantos elementos devem ser retornados.
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
