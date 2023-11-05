7cfb86c4115547f2237056e85fc3d15ba84c6a20
danilo.silva <danilo.silva@msn.com>
2023-11-04 23:13:19 -0300

    Projeto Anterior

---
+++ CriandoWebAPINet6/Cap-04/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,48 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmesController : Controller
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    // É preciso adicionar o atributo [FromBody] para que o ASP.NET Core saiba que o corpo da requisição HTTP deve ser usado para preencher o parâmetro filme.
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+
+        // O método CreatedAtAction() retorna o código 201 (Created) e o cabeçalho Location com a URL do recurso criado.
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        // Skip: Parâmetro que indica quantos elementos devem ser ignorados no início da lista.
+        // Take: Parâmetro que indica quantos elementos devem ser retornados.
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    // O método RecuperaFilmePorId() recebe o id do filme que deve ser retornado e faz uma busca na lista de filmes.
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+        {
+            // NotFound: Retorna o código 404 (Not Found) para o cliente.
+            return NotFound("Filme não encontrado");
+        }
+
+        // Ok: Retorna o código 200 (OK) para o cliente.
+        return Ok(filme);
+    }
+}
---
+++ CriandoWebAPINet6/Cap-04/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo título do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O campo gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O campo gênero do filme não pode ter mais de 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "O campo duração do filme é obrigatório")]
+    [Range(70, 300, ErrorMessage = "A duração deve ter no mínimo 70 e no máximo 300 minutos")]
+    public int Duracao { get; set; }
+}
