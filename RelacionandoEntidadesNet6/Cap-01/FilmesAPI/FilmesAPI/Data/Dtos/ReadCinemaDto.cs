0c126d49ef94d12e73a11c398412db307dc8714e
danilo.silva <danilo.silva@msn.com>
2023-11-05 20:03:41 -0300

    Adicionando o Cinema

---
+++ RelacionandoEntidadesNet6/Cap-01/FilmesAPI/FilmesAPI/Data/Dtos/ReadCinemaDto.cs
@@ -0,0 +1,7 @@
+namespace FilmesAPI.Data.Dtos;
+
+public class ReadCinemaDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
--- RelacionandoEntidadesNet6/Cap-01/FilmesAPI/FilmesAPI/Data/FilmesContext.cs
+++ RelacionandoEntidadesNet6/Cap-01/FilmesAPI/FilmesAPI/Data/FilmesContext.cs
@@ -5,9 +5,8 @@ namespace FilmesAPI.Data;
 
 public class FilmesContext : DbContext
 {
-    public FilmesContext(DbContextOptions<FilmesContext> options) : base(options)
-    {
-    }
+    public FilmesContext(DbContextOptions<FilmesContext> options) : base(options) { }
 
     public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Cinema> Cinemas { get; set; }
 }
