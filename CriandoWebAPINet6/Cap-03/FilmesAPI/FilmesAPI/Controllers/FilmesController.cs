ce7922e362e6ade9afd32528201720b46a161f3c
danilo.silva <danilo.silva@msn.com>
2023-11-04 22:19:46 -0300

    Recuperando filmes

--- CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
+++ CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -8,12 +8,26 @@ namespace FilmesAPI.Controllers;
 public class FilmesController : Controller
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)  // É preciso adicionar o atributo [FromBody] para que o ASP.NET Core saiba que o corpo da requisição HTTP deve ser usado para preencher o parâmetro filme.
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes() // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id) // O método RecuperaFilmePorId() recebe o id do filme que deve ser retornado e faz uma busca na lista de filmes.
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
