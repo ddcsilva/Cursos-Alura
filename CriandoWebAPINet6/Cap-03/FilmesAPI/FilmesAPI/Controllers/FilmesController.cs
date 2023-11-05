1e55170aad6b348e906ceea7b3b7060e57eafdad
danilo.silva <danilo.silva@msn.com>
2023-11-04 23:08:56 -0300

    Padronizando o retorno

--- CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
+++ CriandoWebAPINet6/Cap-03/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -11,25 +11,38 @@ public class FilmesController : Controller
     private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)  // É preciso adicionar o atributo [FromBody] para que o ASP.NET Core saiba que o corpo da requisição HTTP deve ser usado para preencher o parâmetro filme.
+    // É preciso adicionar o atributo [FromBody] para que o ASP.NET Core saiba que o corpo da requisição HTTP deve ser usado para preencher o parâmetro filme.
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
         filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
+
+        // O método CreatedAtAction() retorna o código 201 (Created) e o cabeçalho Location com a URL do recurso criado.
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
     }
 
     [HttpGet]
-    // Skip: Parâmetro que indica quantos elementos devem ser ignorados no início da lista.
-    // Take: Parâmetro que indica quantos elementos devem ser retornados.
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
+    // O método RecuperaFilmes() retorna a lista de filmes que está armazenada na memória do servidor.
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
+        // Skip: Parâmetro que indica quantos elementos devem ser ignorados no início da lista.
+        // Take: Parâmetro que indica quantos elementos devem ser retornados.
         return filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
-    public Filme? RecuperaFilmePorId(int id) // O método RecuperaFilmePorId() recebe o id do filme que deve ser retornado e faz uma busca na lista de filmes.
+    // O método RecuperaFilmePorId() recebe o id do filme que deve ser retornado e faz uma busca na lista de filmes.
+    public IActionResult RecuperaFilmePorId(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+        {
+            // NotFound: Retorna o código 404 (Not Found) para o cliente.
+            return NotFound();
+        }
+
+        // Ok: Retorna o código 200 (OK) para o cliente.
+        return Ok(filme);
     }
 }
