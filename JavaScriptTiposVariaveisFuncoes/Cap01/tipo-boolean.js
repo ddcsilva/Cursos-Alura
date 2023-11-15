e7029ab40383d083cd70479da42c8f485de5ce5a
danilo.silva <danilo.silva@msn.com>
2023-11-15 20:54:24 -0300

    Tipo Boolean

---
+++ JavaScriptTiposVariaveisFuncoes/Cap01/tipo-boolean.js
@@ -0,0 +1,18 @@
+// Tipo Booleano (True/False)
+
+const primeiroNumero = 5;
+console.log(`primeiroNumero: ${primeiroNumero}`);
+const segundoNumero = 10;
+console.log(`segundoNumero: ${segundoNumero}`);
+let cadastroAtivado = true;
+
+console.log(`primeiroNumero === segundoNumero: ${primeiroNumero === segundoNumero}`); // false
+
+console.log('----------------------');
+
+let texto1 = "Alura";
+console.log(`texto1: ${texto1}`);
+let texto2 = "a";
+console.log(`texto2: ${texto2}`);
+
+console.log(`texto1 === texto2: ${texto1 === texto2}`); // false
