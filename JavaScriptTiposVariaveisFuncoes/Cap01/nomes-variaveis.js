08cab24469a31807a89146921af91668fea00b70
danilo.silva <danilo.silva@msn.com>
2023-11-15 20:57:30 -0300

    Nomes de Variáveis

---
+++ JavaScriptTiposVariaveisFuncoes/Cap01/nomes-variaveis.js
@@ -0,0 +1,12 @@
+// Nomes de Variáveis em JavaScript
+const minhaVariavel = 1;
+const MinhaVariavel = "texto";
+const minhavar = "3";
+const MINHAVARIAVEL = 2;
+
+console.log(minhaVariavel, MinhaVariavel, minhavar, MINHAVARIAVEL)
+
+// camelCase: é o padrão adotado pelo JavaScript
+// snake_case: é o padrão adotado pelo Python
+// kebab-case: é o padrão adotado pelo CSS
+// PascalCase: é o padrão adotado pelo C# quando declaramos classes
