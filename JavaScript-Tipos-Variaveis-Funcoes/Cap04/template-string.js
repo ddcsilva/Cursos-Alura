1d303260fe627a313a820bee3070c214ebf73977
danilo.silva <danilo.silva@msn.com>
2023-11-16 10:16:29 -0300

    Template literal

---
+++ JavaScript-Tipos-Variaveis-Funcoes/Cap04/template-string.js
@@ -0,0 +1,9 @@
+const nome = 'Danilo';
+const idade = 2023 - 1989;
+const cidadeDeNascimento = 'Jacareí';
+
+// Concatenação
+console.log('Meu nome é ' + nome + ', tenho ' + idade + ' anos e nasci na cidade de ' + cidadeDeNascimento);
+
+// Template String
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`);
