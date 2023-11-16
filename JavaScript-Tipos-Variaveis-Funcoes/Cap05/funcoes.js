3662df64e172e4eb6516fefc29bef96b729a7880
danilo.silva <danilo.silva@msn.com>
2023-11-16 10:21:52 -0300

    Funções

---
+++ JavaScript-Tipos-Variaveis-Funcoes/Cap05/funcoes.js
@@ -0,0 +1,19 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) Declara a função
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+imprimeTexto("Oi");
+imprimeTexto("Outro texto");
+
+// Três formas de escrever funções
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma());
