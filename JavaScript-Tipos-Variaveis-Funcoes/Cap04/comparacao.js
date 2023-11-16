1dba2f3b39b24c48788694a9bead9ffc500d6fde
danilo.silva <danilo.silva@msn.com>
2023-11-16 10:11:56 -0300

    Operadores de comparação

---
+++ JavaScript-Tipos-Variaveis-Funcoes/Cap04/comparacao.js
@@ -0,0 +1,14 @@
+// == (Comparação Implicita)
+// === (Comparação Explicita)
+const numero = 5;
+const texto = "5";
+
+console.log(`5 == "5": ${numero == texto}`); // true
+console.log(`5 === "5": ${numero === texto}`); // false
+
+// Typeof: Retorna o tipo de dado
+console.log(`typeof numero: ${typeof numero}`);
+console.log(`typeof texto: ${typeof texto}`);
+
+// == compara somente o valor
+// === compara o valor e o tipo de dado
