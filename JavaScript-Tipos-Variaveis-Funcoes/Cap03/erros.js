743648700d55a011b85cffd148fb43f8a6a5a2f8
danilo.silva <danilo.silva@msn.com>
2023-11-16 09:13:24 -0300

    Erros e stacktrace

---
+++ JavaScript-Tipos-Variaveis-Funcoes/Cap03/erros.js
@@ -0,0 +1,15 @@
+// Erro: SyntaxError: Missing initializer in const declaration
+// Significado: Não foi atribuido um valor a constante
+// const numero;
+
+const numero = 456;
+
+// Erro: SyntaxError: Missing ) after argument list
+// Significado: Falta um ) depois da lista de argumentos
+// console.log(numero
+
+console.log(numero);
+
+// Erro: SyntaxError: minhaVar is not defined
+// Significado: minhaVar não foi definida
+// console.log(minhaVar);
