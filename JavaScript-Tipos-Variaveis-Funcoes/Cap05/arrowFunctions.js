ae040c46e46409f6d0ceef706c9a472a49c537e1
danilo.silva <danilo.silva@msn.com>
2023-11-16 10:55:27 -0300

    Arrow Function

---
+++ JavaScript-Tipos-Variaveis-Funcoes/Cap05/arrowFunctions.js
@@ -0,0 +1,11 @@
+function apresentar(nome) {
+    return `meu nome é ${nome}`;
+}
+
+// Arrow Function
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+/* 
+Qual a diferença entre Arrow Function e função tradicional?
+A Arrow Function é sempre uma função anônima, enquanto a função tradicional pode ser anônima ou não.
+*/
