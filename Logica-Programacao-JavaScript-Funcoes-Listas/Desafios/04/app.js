287deb967b29b25fbb3bc6910f036216ab82756b
danilo.silva <danilo.silva@msn.com>
2024-03-05 15:18:18 -0300

    Desafio 04

---
+++ Logica-Programacao-JavaScript-Funcoes-Listas/Desafios/04/app.js
@@ -0,0 +1,15 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+console.log(listaGenerica);
+console.log(linguagensDeProgramacao);
+
+let listaNomes = ['Ana', 'Maria', 'José'];
+
+console.log(`O primeiro nome da lista é: ${listaNomes[0]}`);
+console.log(`O segundo nome da lista é: ${listaNomes[1]}`);
+console.log(`O terceiro nome da lista é: ${listaNomes[2]}`);
