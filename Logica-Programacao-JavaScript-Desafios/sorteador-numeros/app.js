5040bcfa4f8be6a677135c0c7df410777d482a5d
danilo.silva <danilo.silva@msn.com>
2024-03-05 21:36:21 -0300

    Projeto Sorteador de Números

---
+++ Logica-Programacao-JavaScript-Desafios/sorteador-numeros/app.js
@@ -0,0 +1,44 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    let numerosSorteados = [];
+
+    for (let i = 0; i < quantidade; i++) {
+        let numeroSorteado = obterNumeroAleatorio(de, ate);
+
+        while (numerosSorteados.includes(numeroSorteado)) {
+            numeroSorteado = obterNumeroAleatorio(de, ate);
+        }
+
+        numerosSorteados.push(numeroSorteado);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(minimo, maximo) {
+    return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+}
+
+function alterarStatusBotao() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    } else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+    alterarStatusBotao();
+}
