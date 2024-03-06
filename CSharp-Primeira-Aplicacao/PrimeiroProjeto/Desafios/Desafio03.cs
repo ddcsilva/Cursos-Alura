c5fb42f2e35b0faa25c138538830fe553cd69c71
danilo.silva <danilo.silva@msn.com>
2024-03-06 08:14:36 -0300

    Desafio 03

--- CSharp-Primeira-Aplicacao/PrimeiroProjeto/Desafios/Desafio03.cs
+++ CSharp-Primeira-Aplicacao/PrimeiroProjeto/Desafios/Desafio03.cs
@@ -9,12 +9,6 @@ public class Desafio03
         somaElementos();
     }
 
-    /*
-Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro operações básicas (adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
-Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
-Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
-Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
-    */
     public void operacoesBasicas()
     {
         double a = 10;
