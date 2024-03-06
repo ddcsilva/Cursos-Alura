81f6fde61b449353eecb2b4079329f0b1508e712
danilo.silva <danilo.silva@msn.com>
2024-03-06 07:55:52 -0300

    Desafio 01

---
+++ CSharp-Primeira-Aplicacao/PrimeiroProjeto/Desafios/Desafio01.cs
@@ -0,0 +1,14 @@
+namespace Desafios;
+
+public class Desafio01
+{
+    public void iniciar()
+    {
+        string curso = "C#";
+        Console.WriteLine($"Curso de {curso}");
+
+        string nomeInstrutor1 = "Daniel Portugal";
+        string nomeInstrutor2 = "Guilherme Lima";
+        Console.WriteLine($"Instrutores: {nomeInstrutor1} e {nomeInstrutor2}");
+    }
+}
