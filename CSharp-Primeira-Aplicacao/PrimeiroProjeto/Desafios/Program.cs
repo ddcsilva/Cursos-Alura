e01c4019239aafd803779f09f168ec67e48e8d67
danilo.silva <danilo.silva@msn.com>
2024-03-06 07:25:00 -0300

    Desafio 01

---
+++ CSharp-Primeira-Aplicacao/PrimeiroProjeto/Desafios/Program.cs
@@ -0,0 +1,6 @@
+﻿string curso = "C#";
+Console.WriteLine($"Curso de {curso}");
+
+string nomeInstrutor1 = "Daniel Portugal";
+string nomeInstrutor2 = "Guilherme Lima";
+Console.WriteLine($"Instrutores: {nomeInstrutor1} e {nomeInstrutor2}");
