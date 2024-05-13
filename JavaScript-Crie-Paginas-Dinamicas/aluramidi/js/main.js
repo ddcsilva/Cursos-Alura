85d4e92dce01f288186240757d2dcc78ab9c3bbe
danilo.silva <danilo.silva@msn.com>
2024-05-13 19:35:24 -0300

    Clique no botão

--- JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
+++ JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
@@ -1,9 +1,12 @@
 // main.js => Por convenção, o arquivo principal de um projeto é chamado de main.js
 
+// querySelector() => Retorna o primeiro elemento que corresponde a um grupo de seletores CSS.
+
 function tocarSomTeclaPom() {
     // play() => Inicia ou retoma a reprodução do áudio ou vídeo.
     document.querySelector('#som_tecla_pom').play();
 }
 
-// querySelector() => Retorna o primeiro elemento que corresponde a um grupo de seletores CSS.
-document.querySelector('.tecla_pom');
+// É possível atribuir uma função a um evento de um elemento HTML através do atributo onclick.
+// A diferença entre tocarSomTeclaPom e tocarSomTeclaPom() é que a primeira é uma referência à função, enquanto a segunda é a chamada da função.
+document.querySelector('.tecla_pom').onclick = tocarSomTeclaPom;
