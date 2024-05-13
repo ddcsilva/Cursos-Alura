833473107159e8e2f375b5a03fcbea6d5dccfdf0
danilo.silva <danilo.silva@msn.com>
2024-05-13 14:01:42 -0300

    Criando Função

--- JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
+++ JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
@@ -1,6 +1,9 @@
 // main.js => Por convenção, o arquivo principal de um projeto é chamado de main.js
 
+function tocarSomTeclaPom() {
+    // play() => Inicia ou retoma a reprodução do áudio ou vídeo.
+    document.querySelector('#som_tecla_pom').play();
+}
+
 // querySelector() => Retorna o primeiro elemento que corresponde a um grupo de seletores CSS.
 document.querySelector('.tecla_pom');
-// play() => Inicia ou retoma a reprodução do áudio ou vídeo.
-document.querySelector('#som_tecla_pom').play();
