3c51a2b65686d32893bbd554b3b3268edd91a159
danilo.silva <danilo.silva@msn.com>
2024-05-13 13:50:47 -0300

    Buscar um elemento

--- JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
+++ JavaScript-Crie-Paginas-Dinamicas/aluramidi/js/main.js
@@ -1,3 +1,4 @@
 // Por convenção, é nomeado como main.js o arquivo que contém o código principal da aplicação.
 
-alert('Olá Mundo');
+// Retorna o primeiro elemento que corresponde ao seletor especificado. (.tecla_pom)
+document.querySelector('.tecla_pom');
