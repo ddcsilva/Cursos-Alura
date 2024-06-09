b5526c821c3ee76a7a82a80292dffcb98330aa0e
danilo.silva <danilo.silva@msn.com>
2024-06-09 15:56:53 -0300

    Alterando Imagens

--- JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
+++ JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
 const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
+const imagem = document.querySelector('.app__image');
 
 botaoFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    imagem.setAttribute('src', 'imagens/foco.png');
 });
 
 botaoDescansoCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    imagem.setAttribute('src', 'imagens/descanso-curto.png');
 });
 
 botaoDescansoLongo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    imagem.setAttribute('src', 'imagens/descanso-longo.png');
 });
