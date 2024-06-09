ec07b4b73e7a0395c1924c558ba4187dbda7cc8f
danilo.silva <danilo.silva@msn.com>
2024-06-09 16:23:30 -0300

    Adicionando e removendo classes

--- JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
+++ JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
@@ -4,20 +4,25 @@ const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
 const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
 const imagem = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 botaoFoco.addEventListener('click', () => {
     alterarContexto('foco');
+    botaoFoco.classList.add('active');
 });
 
 botaoDescansoCurto.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    botaoDescansoCurto.classList.add('active');
 });
 
 botaoDescansoLongo.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    botaoDescansoLongo.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach(botao => botao.classList.remove('active'));
     html.setAttribute('data-contexto', contexto);
     imagem.setAttribute('src', `imagens/${contexto}.png`);
 
