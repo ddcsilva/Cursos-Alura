57641c06db3f7752b4b45fcc96cd6a8d60d5f6a8
danilo.silva <danilo.silva@msn.com>
2024-06-09 17:38:06 -0300

    Iniciando o temporizador (setInterval)

--- JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
+++ JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
@@ -6,9 +6,11 @@ const imagem = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const inputMusica = document.querySelector('#alternar-musica');
+const botaoIniciarEParar = document.querySelector('#start-pause');
+const musica = new Audio('sons/luna-rise-part-one.mp3').loop = true;
 
-const musica = new Audio('sons/luna-rise-part-one.mp3');
-musica.loop = true;
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 botaoFoco.addEventListener('click', () => {
     alterarContexto('foco');
@@ -51,4 +53,15 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    tempoDecorridoEmSegundos -= 1;
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+}
+
+botaoIniciarEParar.addEventListener('click', contagemRegressiva);
+
+function iniciar() {
+    intervaloId = setInterval(contagemRegressiva, 1000);
 }
