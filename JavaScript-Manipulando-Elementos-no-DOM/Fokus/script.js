2df969fe7eff82082d90da782c36e69efceb1b5b
danilo.silva <danilo.silva@msn.com>
2024-06-09 16:14:52 -0300

    Alterando texto da página

--- JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
+++ JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
@@ -3,6 +3,7 @@ const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
 const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
 const imagem = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 botaoFoco.addEventListener('click', () => {
     alterarContexto('foco');
@@ -19,4 +20,18 @@ botaoDescansoLongo.addEventListener('click', () => {
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     imagem.setAttribute('src', `imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimizar a produtividade, <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
 }
