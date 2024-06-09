d84656b2a30272d4bce521491f548a9f5901500b
danilo.silva <danilo.silva@msn.com>
2024-06-09 15:35:30 -0300

    Tarefa

--- JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
+++ JavaScript-Manipulando-Elementos-no-DOM/Fokus/script.js
@@ -1,6 +1,7 @@
 const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
+const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
 
 botaoFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
@@ -8,4 +9,8 @@ botaoFoco.addEventListener('click', () => {
 
 botaoDescansoCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+});
+
+botaoDescansoLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo');
 });
