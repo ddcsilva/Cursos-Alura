9dc3510868777101b4df09ddc35ddbfeb6927fca
danilo.silva <danilo.silva@msn.com>
2024-06-11 11:41:07 -0300

    Submetendo o formulário

--- JavaScript-Explorando-a-Manipulação-de-Elementos-e-da-LocalStorage/fokus/script-crud.js
+++ JavaScript-Explorando-a-Manipulação-de-Elementos-e-da-LocalStorage/fokus/script-crud.js
@@ -1,6 +1,20 @@
 const botaoAdicionarTarefa = document.querySelector('.app__button--add-task');
+const caixaDeTexto = document.querySelector('.app__form-textarea');
 const formularioAdicionarTarefa = document.querySelector('.app__form-add-task');
+
+const tarefas = [];
 
 botaoAdicionarTarefa.addEventListener('click', function () {
     formularioAdicionarTarefa.classList.toggle('hidden');
+});
+
+formularioAdicionarTarefa.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+
+    const tarefa = {
+        descricao: descricaoTarefa
+    }
+
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
 });
